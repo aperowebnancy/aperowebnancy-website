5433ad3ec141d55f95ab3d63271dac729b2f95a9
zyhou <richard.maxime9@gmail.com>
2020-08-12 00:37:56 +0200

    Add home page draft

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
 import '../styles/tailwind.css';
 
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+
+import { Header } from '../components/Header';
+
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
+            <div className="bg-gray-50 h-full min-h-full">
+                <Header />
+                <main className="py-20 container mx-auto">
+                    <Component {...pageProps} />
+                </main>
+            </div>
+        </>
+    );
 }
 
 MyApp.propTypes = {
