fe0a25fe29d2f84e7d41b60dff55ddb23cef3082
zyhou <richard.maxime9@gmail.com>
2020-08-21 15:26:42 +0200

    generate rss feed

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 const withMDX = require('@next/mdx')();
 
-module.exports = withMDX();
+module.exports = withMDX({
+    webpack: (config, { dev, isServer }) => {
+        if (!dev && isServer) {
+            const originalEntry = config.entry;
+
+            config.entry = async () => {
+                const entries = { ...(await originalEntry()) };
+                entries['./scripts/build-rss.js'] = './scripts/build-rss.js';
+                return entries;
+            };
+        }
+
+        return config;
+    },
+});
