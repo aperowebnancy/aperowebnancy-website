ad32062f8bb89682576b3fd029e7a989526a944b
Luwangel <adrien.amoros@gmail.com>
2020-10-07 10:45:19 +0200

    Move the Youtube component to the components folder

--- components/index.js
+++ components/index.js
@@ -5,3 +5,4 @@ export * from './Header';
 export * from './Icons';
 export * from './Seo';
 export * from './SpeakerLinks';
+export * from './Youtube';
--- pages/talks/[slug].js
+++ pages/talks/[slug].js
@@ -9,34 +9,7 @@ import matter from 'gray-matter';
 import MDX from '@mdx-js/runtime';
 
 import { siteConfig } from '../../lib/siteConfig';
-import { ShareIcon, MeetupIcon, TwitterIcon } from '../../components/Icons';
-import { SpeakerLinks } from '../../components/SpeakerLinks';
-import { Seo } from '../../components/Seo';
-
-function Youtube({ videoId, title }) {
-    return (
-        <figure>
-            <div className="relative pb-9/16">
-                <iframe
-                    title={title}
-                    className="absolute inset-0 h-full w-full"
-                    width="560"
-                    height="315"
-                    src={`https://www.youtube.com/embed/${videoId}`}
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                ></iframe>
-            </div>
-            <figcaption className="italic text-center">{title}</figcaption>
-        </figure>
-    );
-}
-
-Youtube.propTypes = {
-    videoId: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-};
+import { MeetupIcon, Seo, ShareIcon, SpeakerLinks, TwitterIcon, Youtube } from '../../components';
 
 const mdxComponents = {
     Youtube,
