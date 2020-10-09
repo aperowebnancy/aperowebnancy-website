2debcc54fa378442471d614e094d6adf5c82bd2a
zyhou <richard.maxime9@gmail.com>
2020-10-10 01:31:15 +0200

    improve test

--- components/WebMentions.spec.js
+++ components/WebMentions.spec.js
@@ -1,85 +1,34 @@
 import React from 'react';
-import {
-    render,
-    screen,
-    fireEvent,
-    getByText,
-    getByAltText,
-    waitFor,
-} from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 
 import { WebMentions } from './WebMentions';
 
-const mentions = [
-    {
-        name: 'james bond',
-        text: 'Aller voir le super talk',
-        type: 'mentionne',
-        date: 'le 9 oct. 2020 à 21:47:32',
-        image: 'https://james.bond/photo',
-        profileUrl: 'https://james.bond/url',
-        url: 'https://post.url',
-    },
-    {
-        name: 'captain marvel',
-        text: 'bien joué',
-        type: 'répond',
-        date: 'le 7 oct. 2020 à 16:48:38',
-        image: 'https://captain.marvel/photo',
-        profileUrl: 'https://captain.marvel/url',
-        url: 'https://post.url',
-    },
-    {
-        name: 'spider man',
-        text: null,
-        type: 'partage',
-        date: null,
-        image: 'https://spider.man/photo',
-        profileUrl: 'https://spider.man/url',
-        url: 'https://my.url',
-    },
-];
-
 describe('WebMentions Components', () => {
+    describe('Timezones', () => {
+        it('should always be UTC+2', () => {
+            expect(new Date().getTimezoneOffset()).toBe(-120);
+        });
+    });
+
     describe('<WebMentions />', () => {
         it('should render default web mentions', async () => {
-            render(<WebMentions target="https://fake-site.com/take-talk" />);
-
+            render(<WebMentions target="all" />);
             await screen.findByText('1');
 
             expect(screen.getByRole('heading', 'Webmentions')).toBeInTheDocument();
             expect(screen.getByText('1')).toBeInTheDocument(); // 1 j'aime
             expect(screen.getByText('3')).toBeInTheDocument(); // 2 mentions
-
             expect(screen.getByRole('button')).toHaveTextContent('Voir les mentions');
         });
 
-        it('should render full web mentions when click on "Voir les mentions"', async () => {
-            render(<WebMentions target="https://fake-site.com/take-talk" />);
-
+        it('should render "Voir les mentions" button, click it and change text', async () => {
+            render(<WebMentions target="all" />);
             await screen.findByText('1');
+
+            expect(screen.getByRole('button')).toHaveTextContent('Voir les mentions');
+
             fireEvent.click(screen.getByText('Voir les mentions'));
-
             await screen.findByRole('list');
-            expect(screen.getByRole('button')).toHaveTextContent('Cacher les mentions');
-
-            const listWebMentions = screen.getAllByRole('listitem');
-            expect(listWebMentions).toHaveLength(mentions.length);
-
-            mentions.forEach(({ name, text, image, profileUrl, type, url, date }, index) => {
-                const webMention = listWebMentions[index];
-                expect(getByText(webMention, new RegExp(name, 'i'))).toBeInTheDocument();
-
-                if (text) expect(getByText(webMention, text)).toBeInTheDocument();
-                if (date) expect(getByText(webMention, new RegExp(date, 'i'))).toBeInTheDocument();
-
-                const imageTag = getByAltText(webMention, new RegExp(name, 'i'));
-                expect(imageTag).toHaveAttribute('src', image);
-                expect(imageTag.closest('a')).toHaveAttribute('href', profileUrl);
-
-                const typeLink = getByText(webMention, type);
-                expect(typeLink).toHaveAttribute('href', url);
-            });
 
             fireEvent.click(screen.getByText('Cacher les mentions'));
 
@@ -88,6 +37,79 @@ describe('WebMentions Components', () => {
             });
 
             expect(screen.queryByRole('list')).not.toBeInTheDocument();
+        });
+
+        it('should render web mentions of type "mentionne"', async () => {
+            render(<WebMentions target="mention-of" />);
+            await screen.findByText('1');
+
+            fireEvent.click(screen.getByText('Voir les mentions'));
+            await screen.findByRole('list');
+
+            expect(screen.getByText('james bond', { exact: false })).toBeInTheDocument();
+
+            expect(
+                screen.getByText('Aller voir le super talk', { exact: false }),
+            ).toBeInTheDocument();
+            expect(
+                screen.getByText('le 9 oct. 2020 à 21:47:32 :', { exact: false }),
+            ).toBeInTheDocument();
+
+            const imageTag = screen.getByAltText('james bond', { exact: false });
+            expect(imageTag).toHaveAttribute('src', 'https://james.bond/photo');
+            expect(imageTag.closest('a')).toHaveAttribute('href', 'https://james.bond/url');
+
+            const typeLink = screen.getByText('mentionne');
+            expect(typeLink).toHaveAttribute('href', 'https://post.url');
+        });
+
+        it('should render web mentions of type "répond"', async () => {
+            render(<WebMentions target="in-reply-to" />);
+            await screen.findByText('1');
+
+            fireEvent.click(screen.getByText('Voir les mentions'));
+            await screen.findByRole('list');
+
+            expect(screen.getByText('captain marvel', { exact: false })).toBeInTheDocument();
+
+            expect(screen.getByText('bien joué', { exact: false })).toBeInTheDocument();
+            expect(
+                screen.getByText('le 7 oct. 2020 à 16:48:38 :', { exact: false }),
+            ).toBeInTheDocument();
+
+            const imageTag = screen.getByAltText('captain marvel', { exact: false });
+            expect(imageTag).toHaveAttribute('src', 'https://captain.marvel/photo');
+            expect(imageTag.closest('a')).toHaveAttribute('href', 'https://captain.marvel/url');
+
+            const typeLink = screen.getByText('répond');
+            expect(typeLink).toHaveAttribute('href', 'https://post.url');
+        });
+
+        it('should render web mentions of type "partage"', async () => {
+            render(<WebMentions target="repost-of" />);
+            await screen.findByText('1');
+
+            fireEvent.click(screen.getByText('Voir les mentions'));
+            await screen.findByRole('list');
+
+            expect(screen.getByText('spider man', { exact: false })).toBeInTheDocument();
+
+            expect(screen.queryByText('bien joué')).not.toBeInTheDocument();
+            expect(screen.queryByText('le 7 oct. 2020 à 16:48:38 :')).not.toBeInTheDocument();
+
+            expect(screen.queryByRole('image')).not.toBeInTheDocument();
+
+            const typeLink = screen.getByText('partage');
+            expect(typeLink).toHaveAttribute('href', 'https://my.url');
+        });
+
+        it('should render web mentions of type "aime"', async () => {
+            render(<WebMentions target="like-of" />);
+            await screen.findByText('1');
+
+            fireEvent.click(screen.getByText('Voir les mentions'));
+
+            expect(screen.queryByRole('listitem')).not.toBeInTheDocument();
         });
     });
 });
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+    globalSetup: '<rootDir>/jest.global.js',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     transform: {
         '^.+\\.(js)$': '<rootDir>/node_modules/babel-jest',
